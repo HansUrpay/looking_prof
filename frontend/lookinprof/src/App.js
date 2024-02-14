f6c5137b86189e2ec436e08874c9c20700075a12
Leandro <tucholanza@gmail.com>
2024-02-14 20:52:20 -0300

    routes

--- frontend/lookinprof/src/App.js
+++ frontend/lookinprof/src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { BrowserRouter, Routes, Route, Router } from "react-router-dom";
+import Home from './components/Home/Home'
+import Services from './components/Services/Services'
+import Contact from './components/Contact/Contact'
+import Login from './components/Login/Login'
+import Register from './components/Register/Register'
+import NotFound from './components/NotFound/NotFound'
+import NavBar from './components/NavBar/NavBar'
+import Footer from './components/Footer/Footer'
+import ProtectedRoutes from './Routes/ProtectedRoutes'
+import Profile from "./components/Profile/Profile";
+import CheckOut from './components/Checkout/Checkout.jsx'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <NavBar/>
+      <Routes>
+      <Route index element={<Home/>}/>
+      <Route path='contact' element={<Contact/>}/>
+      <Route path='login' element={<Login/>}/>
+      <Route path='register' element={<Register/>}/>
+      {/* <Route path='/*' element={<ProtectedRoutes/>}> */}
+        <Route path='services' element={<Services/>}/>
+        <Route path='profile' element={<Profile/>}/>
+        <Route path='checkout' element={<CheckOut/>}/>
+
+      {/* </Route> */}
+      <Route path='*' element={<NotFound/>}/>
+      </Routes>
+      <Footer/>
+    </BrowserRouter>
   );
 }
 
