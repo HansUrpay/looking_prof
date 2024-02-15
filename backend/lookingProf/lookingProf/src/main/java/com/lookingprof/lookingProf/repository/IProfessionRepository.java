09a02a290c51b585f36b6d05a48c28abea97b522
rubenledesma10 <122803942+rubenledesma10@users.noreply.github.com>
2024-02-15 17:16:39 -0300

    Delete backend/lookingProf/lookingProf directory

--- backend/lookingProf/lookingProf/src/main/java/com/lookingprof/lookingProf/repository/IProfessionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.lookingprof.lookingProf.repository;
-
-import com.lookingprof.lookingProf.model.Profession;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IProfessionRepository extends JpaRepository<Profession, Integer> {
-}
--- backend/lookingProf/lookingProf/src/main/java/com/lookingprof/lookingProf/repository/IUserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.lookingprof.lookingProf.repository;
-
-import com.lookingprof.lookingProf.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IUserRepository extends JpaRepository<User, Integer> {
-}
