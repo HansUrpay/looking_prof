5339b7f97ea7c49697693c6d5e168d6349adc5f1
rubenledesma10 <122803942+rubenledesma10@users.noreply.github.com>
2024-02-15 11:05:59 -0300

    entity, repository, service, controller created

---
+++ backend/lookingProf/lookingProf/src/test/java/com/lookingprof/lookingProf/LookingProfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lookingprof.lookingProf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LookingProfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
