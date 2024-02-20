0965297ae0a876c4309a58024f660d4903a8fa11
jessicaoo-git <jessi-0517@hotmail.com>
2024-02-20 13:37:20 -0300

    styled component

--- frontend/lookinprof/src/components/Login/Login.jsx
+++ frontend/lookinprof/src/components/Login/Login.jsx
@@ -22,55 +22,56 @@ const Login = () => {
   };
   return (
     <div className="flex justify-center">
-      <div className="shadow-2xl rounded-3xl shadow-gray-400 bg-slate-300/10 w-1/3 flex items-center">
-        <img src={man} alt="man with laptop" ></img>
+      <div className="shadow-2xl rounded-3xl shadow-gray-400 bg-slate-200 w-1/3 flex items-center relative left-40">
+        <img src={man} alt="man with laptop" className="z-20"></img>
       </div>
-      
-      <div className="shadow-2xl rounded-3xl shadow-gray-400 px-20 py-20 flex flex-col justify-between w-1/3">
-          <Typography variant="h3" gutterBottom >
+
+      <div className="shadow-2xl rounded-3xl shadow-gray-400 px-20 py-20 flex flex-col justify-between w-1/3 z-10  bg-white">
+        <div className="m-5">
+          <Typography variant="h3" gutterBottom>
             Iniciar Sesión
           </Typography>
-
-          <form className="flex flex-col align-items: center justify-content: center space-y-5">
-            <TextField
-              label="Correo Electrónico"
-              placeholder="Correo Electrónico"
-              variant="outlined"
-            />
-
-            <FormControl variant="outlined">
-              <InputLabel htmlFor="outlined-adornment-password">
-                Password
-              </InputLabel>
-              <OutlinedInput
-                id="outlined-adornment-password"
-                type={showPassword ? "text" : "password"}
-                endAdornment={
-                  <InputAdornment position="end">
-                    <IconButton
-                      aria-label="toggle password visibility"
-                      onClick={handleClickShowPassword}
-                      onMouseDown={handleMouseDownPassword}
-                      edge="end"
-                    >
-                      {showPassword ? <VisibilityOff /> : <Visibility />}
-                    </IconButton>
-                  </InputAdornment>
-                }
-                label="Password"
-              />
-            </FormControl>
-
-            <Button variant="contained">Iniciar Sesión</Button>
-          </form>
-          <p >
-            No tienes una cuenta aún,{" "}
-            <Link to={`/register`} className="text-blue-700 blod font-semibold">
-              click aquí.
-            </Link>
-          </p>
         </div>
-      
+
+        <form className="flex flex-col space-y-5">
+          <TextField
+            label="Correo Electrónico"
+            placeholder="Correo Electrónico"
+            variant="outlined"
+          />
+
+          <FormControl variant="outlined">
+            <InputLabel htmlFor="outlined-adornment-password">
+              Password
+            </InputLabel>
+            <OutlinedInput
+              id="outlined-adornment-password"
+              type={showPassword ? "text" : "password"}
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={handleClickShowPassword}
+                    onMouseDown={handleMouseDownPassword}
+                    edge="end"
+                  >
+                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                  </IconButton>
+                </InputAdornment>
+              }
+              label="Password"
+            />
+          </FormControl>
+
+          <Button variant="contained">Iniciar Sesión</Button>
+        </form>
+        <p>
+          No tienes una cuenta aún,{" "}
+          <Link to={`/register`} className="text-blue-700 blod font-semibold">
+            click aquí.
+          </Link>
+        </p>
+      </div>
     </div>
   );
 };
--- frontend/lookinprof/src/components/Register/Register.jsx
+++ frontend/lookinprof/src/components/Register/Register.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import deliveryMan from "../../assets/deliveryMan.png";
 import FormControl from "@mui/material/FormControl";
 import InputLabel from "@mui/material/InputLabel";
@@ -11,9 +11,6 @@ import Typography from "@mui/material/Typography";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import { Link } from "react-router-dom";
-import FormGroup from "@mui/material/FormGroup";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
 
 const Register = () => {
   const [showPassword, setShowPassword] = React.useState(false);
@@ -38,17 +35,21 @@ const Register = () => {
   };
   return (
     <div className="flex justify-center">
-      <div className="shadow-2xl rounded-3xl shadow-gray-400 bg-slate-300 w-1/3">
-        <img src={deliveryMan} alt="Man delivery"></img>
+      <div className="shadow-2xl rounded-3xl shadow-gray-400 bg-slate-300 w-1/3 relative z-5 left-40">
+        <img
+          src={deliveryMan}
+          alt="Man delivery"
+          className="relative right-32 mt-12"
+        ></img>
       </div>
-     
-        <div className="shadow-2xl rounded-3xl shadow-gray-400 px-20 py-20 h-full flex flex-col justify-between w-1/3">
-          <Typography variant="h3" gutterBottom>
-            Registrarse
-          </Typography>
 
-          <form className="flex flex-col align-items: center justify-content: center space-y-5">
-            {/* <FormGroup>
+      <div className="shadow-2xl rounded-3xl shadow-gray-400 px-20 py-20 flex flex-col justify-between w-1/3 relative left-24 z-10 bg-white">
+        <Typography variant="h3" gutterBottom>
+          Registrarse
+        </Typography>
+
+        <form className="flex flex-col align-items: center justify-content: center space-y-5">
+          {/* <FormGroup>
               <FormControlLabel
                 control={<Checkbox checked={usuarioChecked} onChange={handleUsuarioChange} />}
                 label="Usuario"
@@ -59,58 +60,67 @@ const Register = () => {
               />
             </FormGroup> */}
 
-            {/* <Button variant="outlined" startIcon={<DeleteIcon />}>
+          {/* <Button variant="outlined" startIcon={<DeleteIcon />}>
   Delete
 </Button>
 <Button variant="contained" endIcon={<SendIcon />}>
   Send
 </Button> */}
-            <TextField
-              label="Correo Electrónico"
-              placeholder="Correo Electrónico"
-              variant="outlined"
+          <TextField
+            label="Correo Electrónico"
+            placeholder="Correo Electrónico"
+            variant="outlined"
+            required
+          />
+
+          <TextField
+            label="Nombres"
+            required
+            placeholder="Nombres"
+            variant="outlined"
+          />
+
+          <TextField
+            label="Apellidos"
+            required
+            placeholder="Apellidos"
+            variant="outlined"
+          />
+
+          <FormControl variant="outlined">
+            <InputLabel htmlFor="outlined-adornment-password">
+              Password
+            </InputLabel>
+            <OutlinedInput
+              id="outlined-adornment-password"
+              type={showPassword ? "text" : "password"}
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={handleClickShowPassword}
+                    onMouseDown={handleMouseDownPassword}
+                    edge="end"
+                  >
+                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                  </IconButton>
+                </InputAdornment>
+              }
+              label="Password"
             />
+          </FormControl>
 
-            <TextField
-              label="Nombre y Apellido"
-              required
-              placeholder="Nombre y Apellido"
-              variant="outlined"
-            />
-
-            <FormControl variant="outlined">
-              <InputLabel htmlFor="outlined-adornment-password">
-                Password
-              </InputLabel>
-              <OutlinedInput
-                id="outlined-adornment-password"
-                type={showPassword ? "text" : "password"}
-                endAdornment={
-                  <InputAdornment position="end">
-                    <IconButton
-                      aria-label="toggle password visibility"
-                      onClick={handleClickShowPassword}
-                      onMouseDown={handleMouseDownPassword}
-                      edge="end"
-                    >
-                      {showPassword ? <VisibilityOff /> : <Visibility />}
-                    </IconButton>
-                  </InputAdornment>
-                }
-                label="Password"
-              />
-            </FormControl>
-
-            <Button variant="contained" className="shadow-2xl">Registrarme</Button>
-          </form>
-          <p className='pt-5'>
-            Ya tienes una cuenta,{" "}
-            <Link to={`/login`} className="text-blue-700 blod font-semibold ">
-              click aquí.
-            </Link>
-          </p>
-        </div>
-      
+          <Button variant="contained" className="shadow-2xl">
+            Registrarme
+          </Button>
+        </form>
+        <p className="pt-5">
+          Ya tienes una cuenta,{" "}
+          <Link to={`/login`} className="text-blue-700 blod font-semibold ">
+            click aquí.
+          </Link>
+        </p>
+      </div>
     </div>
   );
 };
