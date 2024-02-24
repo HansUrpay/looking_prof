6bb8145505af0283696970b4ae45be53c3a940cd
Elian Bargaz <elibargaz@hotmail.com>
2024-02-24 14:57:53 -0300

    Endpoints user

--- backend/lookingProf/src/main/java/com/lookingprof/lookingProf/service/IUserService.java
+++ backend/lookingProf/src/main/java/com/lookingprof/lookingProf/service/IUserService.java
@@ -10,6 +10,7 @@ import com.lookingprof.lookingProf.model.Province;
 import com.lookingprof.lookingProf.model.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -19,12 +20,13 @@ public interface IUserService extends UserDetailsService {
     public Optional<List<UserResponseDTO>> listAll();
     public Optional<User> findById(Integer id);
     public Optional<User> deleteUser(Integer id);
-    public Optional<UserResponseDTO> updateUser(Integer id, User user);
-    public Optional<List<UserResponseDTO>> findByFirstname(String userName);
+    public Optional<UserResponseDTO> updateUser(Integer id, UserResponseDTO userDTO);
+    public Optional<List<UserResponseDTO>> findByFirstname(String firstName);
     public Optional<UserResponseDTO> findByEmail(String email);
-    public Optional<List<UserResponseDTO>> findByProfession(Profession profession);
-    public Optional<List<UserResponseDTO>> findByProvince(Province province);
-    public Optional<List<UserResponseDTO>> findByCity(City city);
+
+    public Optional<List<UserResponseDTO>> findByProfession(String profession);
+    public Optional<List<UserResponseDTO>> findByProvince(String province);
+    public Optional<List<UserResponseDTO>> findByCity(String city);
     public Optional<List<UserResponseDTO>> findByQualification(int qualification);
 
     public AuthResponse loginUser(LoginRequest user);
