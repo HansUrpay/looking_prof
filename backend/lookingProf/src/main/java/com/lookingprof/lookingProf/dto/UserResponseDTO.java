ffaca8edae9945939d251717357b1d842f797fc1
Elian Bargaz <elibargaz@hotmail.com>
2024-02-28 01:40:03 -0300

    Update user

--- backend/lookingProf/src/main/java/com/lookingprof/lookingProf/dto/UserResponseDTO.java
+++ backend/lookingProf/src/main/java/com/lookingprof/lookingProf/dto/UserResponseDTO.java
@@ -22,9 +22,9 @@ public class UserResponseDTO {
     private String lastName;
     private String imageUrl;
     private String phone;
-    //private String  profession;
-    //private String province;
-    //private String city;
+    private String  profession;
+    private String province;
+    private String city;
 
 
     public UserResponseDTO(User user){
@@ -36,9 +36,9 @@ public class UserResponseDTO {
         this.lastName= user.getLastName();
         this.imageUrl= user.getImageUrl();
         this.phone=user.getPhone();
-        //this.profession= user.getProfession().getNameProfession();
-        //this.province= user.getProvince().getNameProvince();
-        //this.city= user.getCity().getNameCity();
+        this.profession= user.getProfession().getNameProfession();
+        this.province= user.getProvince().getNameProvince();
+        this.city= user.getCity().getNameCity();
     }
 
 }
