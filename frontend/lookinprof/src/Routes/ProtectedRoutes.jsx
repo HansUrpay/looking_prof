207266cf78bbea647491d33eb9902e19b84275a2
HansUrpay <hansurpayh@gmail.com>
2024-04-07 18:43:23 -0500

    fix: currentUser in ProtectedRoutes

--- frontend/lookinprof/src/Routes/ProtectedRoutes.jsx
+++ frontend/lookinprof/src/Routes/ProtectedRoutes.jsx
@@ -3,8 +3,8 @@ import { Navigate, Outlet } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
 const ProtectedRoutes = () => {
-    //const { currentUser } = useSelector(state => state.user);
-    const { currentUser } = useSelector(state => state.userEntity);
+    const { currentUser } = useSelector(state => state.user);
+    // const { currentUser } = useSelector(state => state.userEntity);
     return currentUser ? <Outlet/> : <Navigate to='login'/>;
 }
 
